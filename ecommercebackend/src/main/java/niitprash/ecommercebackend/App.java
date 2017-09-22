27ea3045b4e6617360e6630c9bbf7b834df691d5
PrashanthiRamesh <rvprashanthi@gmail.com>
2017-09-22 12:27:45 +0530

    initial commit

---
+++ ecommercebackend/src/main/java/niitprash/ecommercebackend/App.java
@@ -0,0 +1,13 @@
+package niitprash.ecommercebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
