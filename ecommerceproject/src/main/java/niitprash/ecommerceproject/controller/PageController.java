5317dd6f39963e68710f4744f0b96916bd061637
PrashanthiRamesh <rvprashanthi@gmail.com>
2017-09-22 21:16:27 +0530

    Static Landing Page

--- ecommerceproject/src/main/java/niitprash/ecommerceproject/controller/PageController.java
+++ ecommerceproject/src/main/java/niitprash/ecommerceproject/controller/PageController.java
@@ -13,4 +13,24 @@ public class PageController {
 			mv.addObject("msg","Hey You! :)");
 			return mv;
 		}
+		
+//		@RequestMapping(value="/test")
+//		public ModelAndView test(@RequestParam(value="msg", required=false)String msg) {
+//			if(msg==null) {
+//				msg="Prash";
+//			}
+//			ModelAndView mv=new ModelAndView("page");
+//			mv.addObject("msg",msg);
+//			return mv;
+//		}
+		
+//		@RequestMapping(value="/test/{msg}")
+//		public ModelAndView test(@PathVariable("msg")String msg) {
+//			if(msg==null) {
+//				msg="Prash";
+//			}
+//			ModelAndView mv=new ModelAndView("page");
+//			mv.addObject("msg",msg);
+//			return mv;
+//		}
 }
